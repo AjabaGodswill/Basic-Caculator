a6ea382a5db91086dff97abdc8601dd283ceee29
AjabaGodswill <AjabaGodswill@gmail.com>
2021-07-27 13:12:28 +0100

    Second commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 function insert(num){
-    document.form.calculatorScreen.value=document.form.calculatorScreen.value+num
+    document.form.calculatorInput.value=document.form.calculatorInput.value+num
 }
 
 function equal(){
-    var exp =  document.form.calculatorScreen.value;        
+    var exp =  document.form.calculatorInput.value;        
     if(exp){
-       document.form.calculatorScreen.value = eval(exp)
+       document.form.calculatorInput.value = eval(exp)
     }
 }
 
